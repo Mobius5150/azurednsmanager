1bcb603f577b6b2c744d73e85a35007a98904100
Mobius5150 <mike@michaelblouin.ca>
2015-10-22 23:25:06 -0700

    Have DNS record and record set creation working.

--- spec/recordSetSpec.js
+++ spec/recordSetSpec.js
@@ -17,9 +17,10 @@ describe("RecordSet", function() {
 
 		expect(function() { new RecordSet("", "", "") }).toThrowError(/type/);
 		expect(function() { new RecordSet("", "", "", null) }).toThrowError(/type/);
+		expect(function() { new RecordSet("", "", "", 'unknownType') }).toThrowError(/type/);
 
-		expect(function() { new RecordSet("", "", "", "") }).toThrowError(/path/);
-		expect(function() { new RecordSet("", "", "", "", null) }).toThrowError(/path/);
+		expect(function() { new RecordSet("", "", "", "A") }).toThrowError(/path/);
+		expect(function() { new RecordSet("", "", "", "A", null) }).toThrowError(/path/);
 	});
 
 	it("construct from object", function() {
@@ -37,5 +38,23 @@ describe("RecordSet", function() {
 
 		expect(typeof createdObj.properties.ttl).toBe('number');
 		expect(createdObj.properties.ttl > 0).toBe(true);
+	});
+
+	it("should be able to add properties", function() {
+		var constructObject = {
+			'name': '@',
+			'type': 'A',
+			'location': 'global',
+			'tags': [],
+			'eTag': 'myETag',
+			'id' : 'myId',
+		};
+
+		var createdObj = new RecordSet(constructObject);
+		expect(createdObj).toEqual(jasmine.objectContaining(constructObject));
+
+		createdObj.AddProperty({ ipv4Address: '192.168.0.255' });
+		expect(createdObj.properties.aRecords.length).toEqual(1);
+		expect(createdObj.properties.aRecords[0].ipv4Address).toBe('192.168.0.255');
 	});
 });
