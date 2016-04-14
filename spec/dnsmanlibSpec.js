a19f8ab894584fce377d389e46506957b2cb2df6
Mike Blouin <mike@michaelblouin.ca>
2016-04-14 10:57:50 -0600

    Update for new Azure CLI typenames

--- spec/dnsmanlibSpec.js
+++ spec/dnsmanlibSpec.js
@@ -1,7 +1,35 @@
 var lib = require('../lib/dnsmanlib.js');
 
 describe("lib", function() {
-  it("init", function() {
-    expect(typeof lib.init).toBe('function');
-  });
+	it("init", function() {
+		expect(typeof lib.init).toBe('function');
+	});
+});
+
+describe("lib-azure-interface", function() {
+	var lib;
+
+	beforeEach(function() {
+		var openlib = require('../lib/dnsmanlib.js');
+
+		lib = openlib.init({//dnsManager.recordSets.createOrUpdate
+			dnsManager: {
+				recordSets: {
+					createOrUpdate: function () { console.log("Called!"); },
+					listAll: function (group, zone) { console.log('listAll', group, zone); },
+				}
+			},
+		}, {}, null);
+	});
+
+	it("init", function () {
+
+	});
+
+	it("getAzureDNSRecords", function () {
+		// function getAzureDNSRecords(resourceGroup, zoneName, callback) {
+		lib.getAzureDNSRecords('testGroup', 'testZoneName', function () {
+			console.log('Received azure dns records');
+		})
+	});
 });
